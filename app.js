976a48cb196cc3e1493f42c40024ac01b79b1735
P00LY <alexjmarsh55@gmail.com>
2022-03-23 10:10:55 -0700

    fix color selector, grid resizing, removeCell func

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const colorSelector = document.getElementById("colorSelector");
-
 const default_size = '16'
 const default_color = '#000000'; //set default color to black
 
 
 //Create Grid 
 const container_div = document.getElementById("grid-container");
+const colorSelector = document.getElementById("colorSelector");
 
-function makeGrid(rows) { 
-    container_div.style.setProperty('--grid-rows', rows);
-    container_div.style.setProperty('--grid-cols', rows);
-    
-    for (let i = 0; i < (rows * rows); i++) {
+function makeGrid() { 
+    for (let i = 0; i < 256; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-item");
+        cell.addEventListener('mouseover', colorSelect);
         container_div.appendChild(cell);
-        cell.addEventListener('mouseover', function (event) {
-            event.target.style.backgroundColor = colorSelector.value;
-        })
     };
+};
+
+//Color Selector
+function colorSelect(event) {
+    event.target.style.backgroundColor = colorSelector.value;
 };
 
 
@@ -33,35 +32,39 @@ function updateSliderValue(value) {
 
 //Remove previous grid for changing grid size
 function removeCells(parent) {
-    while (parent.firschild) {
+    while(parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 };
 
 //Change grid size
-sizeSlider_input.addEventListener('input', function () {
-    let value = sizeSlider_input.value //grabs value attribute of this input element
+sizeSlider_input.addEventListener('input', changeSize)
+
+
+function changeSize() {
+    const value = sizeSlider_input.value;
+    clearFunction();
     removeCells(container_div);
-    container_div.setAttribute('style', `grid-template-solumns: repeat(${value},
-        1fr); grid-template-rows: repeat(${value}, 1fr);`);
-    for (let i = 0; i < value * value; i++) {
+    container_div.setAttribute('style', `grid-template-columns: repeat(${value},
+        2fr); grid-template-rows: repeat(${value}, 2fr);`);
+    for (let i = 0; i < (value * value); i++) {
         const cell = document.createElement('div');
         cell.classList.add('grid-item');
-        container_div.appendChild(cell);
         cell.addEventListener('mouseover', function (event) {
             event.target.style.backgroundColor = colorSelector.value
         })
+        container_div.appendChild(cell);
     }
-});
+};
 
 //Clear grid button
-const value = sizeSlider_input.value
-cell = container_div.children;
 const clear = document.getElementById('clearbtn');
 clear.addEventListener('click', clearFunction);
 
 function clearFunction() {
-    for (let i = 0; i < value * value; i++) {
+    cell = container_div.children;
+    let value = sizeSlider_input.value;
+    for (let i = 0; i < cell.length; i++) {
         cell[i].style.backgroundColor = 'white';
     }
 };
@@ -71,8 +74,10 @@ const rainbow = document.getElementById('rainbowbtn');
 rainbow.addEventListener('click', rainbowfunc)
 
 function rainbowfunc() {
+    let value = sizeSlider_input.value;
+    const cell = container_div.children;
     for (let i = 0; i < value * value; i++)
-        cell[i].addEventListener('mouseover', rainbowBackground);  
+        cell[i].addEventListener('mouseover', rainbowBackground); 
 };
 
 function rainbowBackground(event) {
@@ -88,7 +93,22 @@ function randomColor() {
     return color;
 };
 
+//Color mode button
+const colorMode = document.getElementById('colorbtn');
+colorMode.addEventListener('click', colorModeFunc);
+function colorModeFunc() {
+    let value = sizeSlider_input.value;
+    let cell = container_div.children;
+    for (let i = 0; i < value * value; i++){
+        cell[i].addEventListener('mouseover', setInk)
+    }
+};
 
+function setInk(event) {
+    event.target.style.backgroundColor = colorSelector.value;
+}
+
+//Load default size grid
 window.onload = () => {
     makeGrid(default_size);
-};
+}
